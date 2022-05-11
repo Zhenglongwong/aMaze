3a93c60506428d0b969a644b578d439376c4f4d0
wongzl <zhenglong.wong@gmail.com>
2022-05-11 14:00:59 +0800

    add premade maps

--- main.js
+++ main.js
@@ -5,29 +5,32 @@ import $ from 'jquery'
 const $app = $('#app');
 
 //initial setup
-
-const state = {
+const BASESTATE = {
   draw: false,
   move: false,
   selectStart: false,
   selectEnd: false,
+  delete: false,
   direction: "",
   player_loc_x: 0,
   player_loc_y: 0,
+  started: false
 }
 
-const createboxes = () => {
-  for (let i = 1; i <= 20; i++) {
-    for (let j = 1; j <= 20; j++) {
+let state = { ...BASESTATE }
+
+const createBoxes = () => {
+  for (let i = 1; i <= 18; i++) {
+    for (let j = 1; j <= 18; j++) {
       $app.append($("<div>").attr('x',`${j}`).attr('y',`${i}`).addClass('box'))
     }
   }
 }
 
-createboxes()
+createBoxes()
 
-const mask = () => {
-  $('.box').addClass('mask')
+const unmask = () => {
+  $(`[x=${state.player_loc_x}][y=${state.player_loc_y}]`).removeClass('mask')
   $(`[x=${state.player_loc_x + 1}][y=${state.player_loc_y}]`).removeClass('mask')
   $(`[x=${state.player_loc_x - 1}][y=${state.player_loc_y}]`).removeClass('mask')
   $(`[x=${state.player_loc_x}][y=${state.player_loc_y + 1}]`).removeClass('mask')
@@ -42,6 +45,12 @@ const mask = () => {
 const drawMaze = (event) => {
   if (state.draw) {
     $(event.target).addClass('selected')
+  }
+}
+
+const deleteItems = (event) => {
+  if (state.delete) {
+    $(event.target).removeClass('selected player end')
   }
 }
 
@@ -65,14 +74,16 @@ const updatePosition = () => {
   const movePlayer = () => {
     $('.player').removeClass('player')
     $(`[x=${state.player_loc_x}][y=${state.player_loc_y}]`).addClass('player')
-    mask()
+    unmask()
     if ($(`[x=${state.player_loc_x}][y=${state.player_loc_y}]`).attr('class').includes('end')) {
       alert('You win!')
     }
   }
 
   const validPath = (x, y) => {
-    return(!$(`[x=${x}][y=${y}]`).attr('class').includes('selected'))
+    try {
+      return(!$(`[x=${x}][y=${y}]`).attr('class').includes('selected'))
+    } catch (err) {}
   }
 
   switch (state.direction) {
@@ -106,55 +117,101 @@ const updatePosition = () => {
       break;
     default:
       break;
-  }
+  } unmask()
 }
 let intervalID;
 const toggleMovement = () => {
+  if ($('.player').length === 0 || $('.end').length === 0) {
+    alert('Please select a start point and end point.')
+    return
+  }
   state.draw = false
   state.selectStart = false
   state.selectEnd = false
   state.move = !state.move
-  mask()
+  if (!state.started) {
+    $('.box').addClass('mask')
+    state.started = true
+  }
   if (state.move) {
-    intervalID = setInterval(updatePosition, 500)
+    intervalID = setInterval(updatePosition, 300)
   } else if (!state.move) {
     clearInterval(intervalID)
   }
 }
 
-const changeDirection = (event) => {
-  event.preventDefault()
-  state.direction = event.key
+const handlePress = (event) => {
+  switch (event.key) {
+    case 'ArrowUp':
+    case 'ArrowDown':
+    case 'ArrowLeft':
+    case 'ArrowRight':
+      event.preventDefault()
+      state.direction = event.key
+      break;
+    case 'f':
+      toggleDraw()
+      break;
+    case 'd':
+      toggleDelete()
+      break;
+  }
 } 
 
-window.addEventListener('keydown', changeDirection)
+window.addEventListener('keydown', handlePress)
 
 //toggles that change state for the various actions
 //eventhandlers for buttons
 const toggleDraw = () => {
   state.selectStart = false
   state.selectEnd = false
+  state.delete = false
   state.draw = !state.draw;
 }
 
 
 const toggleStartPoint = () => {
   state.draw = false
-
+  state.delete = false
   state.selectEnd = false
   state.selectStart = !state.selectStart
 }
 
 const toggleEnd = () => {
   state.draw = false;
-
+  state.delete = false
   state.selectStart = false;
   state.selectEnd = !state.selectEnd;
 }
 
+const reset = () => {
+  $('.box').removeClass('selected player end mask')
+  state = { ...BASESTATE }
+  clearInterval(intervalID)
+}
 
-$('.box').on('mouseover', drawMaze).on('click', createEnd).on('click', createStart)
+const toggleDelete = () => {
+  state.draw = false
+  state.selectStart = false
+  state.selectEnd = false
+  state.delete = !state.delete
+}
+
+const loadMap = (str) => {
+  $('#app').empty()
+  reset()
+  $('#app').load(`${str}.html`, function () {
+    state.player_loc_x = parseInt($('.player').attr('x'))
+    state.player_loc_y = parseInt($('.player').attr('y'))
+    toggleMovement()
+  })
+}
+
+$('.box').on('mouseover', drawMaze).on('click', createEnd).on('click', createStart).on('click', deleteItems)
 $('#draw').on('click', toggleDraw)
+$('#delete').on('click', toggleDelete)
 $('#start').on('click', toggleMovement)
 $('#selectEnd').on('click', toggleEnd)
 $('#selectStart').on('click', toggleStartPoint)
+$('#reset').on('click', reset)
+$('#map1').on('click', () => { loadMap('map1') })
